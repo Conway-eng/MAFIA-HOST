e4fc1e35d6157477fabcbb0727164db219b06504
HACKLINK-XD <krugger99@yahoo.com>
2025-07-20 03:28:59 -0700

    Update emailConnection.js

--- api/database/emailConnection.js
+++ api/database/emailConnection.js
@@ -11,12 +11,12 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
-    host: 'mxslurp.click',
-    port: 2525, 
-    secure: true,
-    auth: {
-        user: '4Dz4YVEOkx3zd77eZ0ERwqyC3xojor2X',
-        pass: 'pa96AaB7ZIHrrxL1qD8GWBOshZnGgEfk'
+ host: "smtp.ethereal.email",
+  port: 587,
+  secure: true,
+  auth: {
+    user: "maddison53@ethereal.email",
+    pass: "jn7jnAPss4f63QBp6D",
     }
 });
 
