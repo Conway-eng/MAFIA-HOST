f338e10267e14b4d88c4be16f030196c38d6895b
Conway-eng <Mornah188@gmail.com>
2025-07-22 02:40:40 -0700

    Update emailConnection.js

--- api/database/emailConnection.js
+++ api/database/emailConnection.js
@@ -11,12 +11,12 @@ const nodemailer = require('nodemailer');
 
 
 const transporter = nodemailer.createTransport({
- host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // true for 465, false for other ports
-  auth: {
-    user: "maddison53@ethereal.email",
-    pass: "jn7jnAPss4f63QBp6D",
+  host: 'mail.spacemail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'msg@talkdrove.com',
+        pass: '3800380@Hamza'
       }
 });
 
